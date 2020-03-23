e727d45e80c5f86951060f7d3f6cc2e24c692f93
joanna <joanna.polewczyk@o2.pl>
2020-03-23 22:36:03 +0100

    connect to flask

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import pandas as pandas
+import plotly.express as px
 from flask import Flask, render_template, request
 import runner as r
 
@@ -10,6 +12,16 @@ def index():
 @app.route('/temp')
 def contruction():
     return render_template('construction.html')
+
+@app.route('/plotly')
+def plotly():
+    df = px.data.gapminder()
+
+    fig = px.scatter(df.query("year==2007"), x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent",
+                     text="country", log_x=True, size_max=60)
+    return fig.show()
+
 
 @app.route('/', methods=['POST'])
 def my_form_post():
