ac634a0f134ab45bdf74c5f9767a77c9aa814013
Jan Scheffczyk <knork@m1234.de>
2020-03-26 20:45:20 +0100

    extract plotting

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 from flask import Flask, render_template, request
 from nltk.corpus import wordnet as wn
 
-import utils.balls_to_json as util
+import utils.plotly as util
 import runner as r
 
 app = Flask(__name__)
@@ -25,52 +25,7 @@ def contruction():
 
 @app.route('/plotly')
 def plotly():
-    ball = util.balls_to_object("out/test1/reduced_nballs_after.txt")
-
-    fig = go.Figure()
-
-    # Create scatter trace of text labels
-    # fig.add_trace(go.Scatter(
-    #     x=[1.5, 3.5],
-    #     y=[0.75, 2.5],
-    #     text=["Unfilled Circle",
-    #           "Filled Circle"],
-    #     mode="text",
-    # ))
-
-    # Set axes properties
-    # fig.update_xaxes(range=[0, 4.5], zeroline=False)
-    # fig.update_yaxes(range=[0, 4.5])
-
-    circles = []
-    for b in ball:
-        circles.append(dict(
-            type="circle",
-            xref="x",
-            yref="y",
-            x0=b.vector[0] - b.radius,
-            y0=b.vector[1] - b.radius,
-            x1=b.vector[0] + b.radius,
-            y1=b.vector[1] + b.radius,
-            line_color="LightSeaGreen",
-        ))
-
-    # Add circles
-    fig.update_layout(
-        shapes=circles,
-        yaxis=dict(
-            scaleanchor="x",
-            scaleratio=1,
-        ),
-        xaxis_showgrid=False,
-        yaxis_showgrid=False,
-
-    )
-
-    # Set figure size
-    # fig.update_layout(width=800, height=800)
-
-    return fig.show(config={'scrollZoom': True})
+   return util.plot_balls()
 
 
 @app.route('/', methods=['POST'])
---
+++ utils/plotly.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+import utils.balls_to_json as util
+
+def plot_balls():
+    ball = util.balls_to_object("out/test1/reduced_nballs_after.txt")
+
+    fig = go.Figure()
+
+    # Create scatter trace of text labels
+    # fig.add_trace(go.Scatter(
+    #     x=[1.5, 3.5],
+    #     y=[0.75, 2.5],
+    #     text=["Unfilled Circle",
+    #           "Filled Circle"],
+    #     mode="text",
+    # ))
+
+    # Set axes properties
+    # fig.update_xaxes(range=[0, 4.5], zeroline=False)
+    # fig.update_yaxes(range=[0, 4.5])
+
+    circles = []
+    for b in ball:
+        circles.append(dict(
+            type="circle",
+            xref="x",
+            yref="y",
+            x0=b.vector[0] - b.radius,
+            y0=b.vector[1] - b.radius,
+            x1=b.vector[0] + b.radius,
+            y1=b.vector[1] + b.radius,
+            line_color="LightSeaGreen",
+        ))
+
+    # Add circles
+    fig.update_layout(
+        shapes=circles,
+        yaxis=dict(
+            scaleanchor="x",
+            scaleratio=1,
+        ),
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+
+    )
+
+    # Set figure size
+    # fig.update_layout(width=800, height=800)
+
+    return fig.show(config={'scrollZoom': True})
