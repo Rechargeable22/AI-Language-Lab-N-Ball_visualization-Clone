93eafd34ab7d4e026b990b4b55a4a2766863c21d
joanna <joanna.polewczyk@o2.pl>
2020-03-26 19:53:13 +0100

    text and color to the ball graph

--- utils/plotly.py
+++ utils/plotly.py
@@ -1,53 +1,60 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
 import utils.balls_to_json as util
 
+
 def plot_balls():
-    ball = util.balls_to_object("out/test1/reduced_nballs_after.txt")
+    def plotly():
+        ball = util.balls_to_object("out/test1/reduced_nballs_after.txt")
 
-    fig = go.Figure()
+        df = pd.DataFrame([t.__dict__ for t in ball])
+        df[['x', 'y']] = pd.DataFrame(df.vector.values.tolist(), index=df.index)
+        print(df.to_string())
+        df['x0'] = df.x - df.radius
+        df['x1'] = df.x + df.radius
+        df['y0'] = df.y - df.radius
+        df['y1'] = df.y + df.radius
+        print(df.to_string())
 
-    # Create scatter trace of text labels
-    # fig.add_trace(go.Scatter(
-    #     x=[1.5, 3.5],
-    #     y=[0.75, 2.5],
-    #     text=["Unfilled Circle",
-    #           "Filled Circle"],
-    #     mode="text",
-    # ))
+        final_df = df.sort_values(by=['radius'], ascending=False)
+        print(final_df.to_string())
 
-    # Set axes properties
-    # fig.update_xaxes(range=[0, 4.5], zeroline=False)
-    # fig.update_yaxes(range=[0, 4.5])
+        fig = go.Figure()
 
-    circles = []
-    for b in ball:
-        circles.append(dict(
-            type="circle",
-            xref="x",
-            yref="y",
-            x0=b.vector[0] - b.radius,
-            y0=b.vector[1] - b.radius,
-            x1=b.vector[0] + b.radius,
-            y1=b.vector[1] + b.radius,
-            line_color="LightSeaGreen",
+        fig.add_trace(go.Scatter(
+            x=final_df["x"],
+            y=final_df["y0"],
+            text=final_df["word"],
+            mode="text",
         ))
 
-    # Add circles
-    fig.update_layout(
-        shapes=circles,
-        yaxis=dict(
-            scaleanchor="x",
-            scaleratio=1,
-        ),
-        xaxis_showgrid=False,
-        yaxis_showgrid=False,
+        circles = []
+        for b in ball:
+            color = np.random.choice(range(256), size=3)
+            circles.append(dict(
+                type="circle",
+                xref="x",
+                yref="y",
+                x0=b.vector[0] - b.radius,
+                y0=b.vector[1] - b.radius,
+                x1=b.vector[0] + b.radius,
+                y1=b.vector[1] + b.radius,
+                line_color="rgb(" + str(color[0]) + ", " + str(color[1]) + ", " + str(color[2]) + ")",
+            ))
 
-    )
+        # Add circles
+        fig.update_layout(
+            shapes=circles,
+            yaxis=dict(
+                scaleanchor="x",
+                scaleratio=1,
+            ),
+            xaxis_showgrid=False,
+            yaxis_showgrid=False,
 
-    # Set figure size
-    # fig.update_layout(width=800, height=800)
+        )
 
-    return fig.show(config={'scrollZoom': True})
+        return fig.show(config={'scrollZoom': True})
