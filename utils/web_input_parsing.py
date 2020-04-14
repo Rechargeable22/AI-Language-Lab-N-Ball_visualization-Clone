814b65300663cd61f58ab975f53351113c50fe9f
Rechargeable22 <oliver.leuschner@gmx.de>
2020-04-14 11:09:51 +0200

    added doku :O

--- utils/web_input_parsing.py
+++ utils/web_input_parsing.py
@@ -1,21 +1,3 @@
-import json
-
-words_paths = ['*root* *root* \n',
-               'animal *root* animal \n',
-               'human *root* animal human \n',
-               'socrates *root* animal human socrates \n']
-maybe_json = """[{
-	"node": "animal",
-	"children": [{
-		"node": "human",
-		"children": [{
-			"node": "socrates",
-			"children": []
-		}]
-	}]
-}]"""
-
-
 def _tree2paths(tree, paths, current_path):
     current_path += tree["node"] + " "
     paths.append(tree["node"] + " *root* " + current_path + "\n")
@@ -23,8 +5,7 @@ def _tree2paths(tree, paths, current_path):
         _tree2paths(child, paths, current_path)
 
 
-def json_to_paths(input_json):
-    trees = json.loads(input_json)
+def dict_to_paths(trees):
     paths = []
     paths.append('*root* *root* \n')
     for tree in trees:
@@ -33,43 +14,43 @@ def json_to_paths(input_json):
     return paths
 
 
-
-
-
-
-# input string to json
-text = "human is animal, socrates is human, kant is human"
-text2 = "human is not animal, socrates is human"
-
-def input_text_to_dict(input_text):
-    out = {}
+def input_text_to_path(input_text):
+    """
+    Takes text input in the form of simple sentences: CHILD is [NOT] PARENT
+    Parses it to a "tree paths" representation that is used in the paper.
+    "human is not animal, socrates is human"
+    :param input_text: "human is animal, socrates is human, kant is human"
+    :return: words_paths = ['*root* *root* \n',
+                   'animal *root* animal \n',
+                   'human *root* animal human \n',
+                   'socrates *root* animal human socrates \n']
+    """
     entities = []
     texts = input_text.split(",")
-    has_parent=[]
+    has_parent = []
     for text in texts:
         words = text.split()
         child = words[0]
         parent = words[-1]
 
-        child_node=next((x for x in entities if x["name"]==child), None)
+        child_node = next((x for x in entities if x["node"] == child), None)
         if not child_node:
-            child_node={"name":child,"children":[]}
+            child_node = {"node": child, "children": []}
             entities.append(child_node)
 
-        parent_node = next((x for x in entities if x["name"] == parent), None)
+        parent_node = next((x for x in entities if x["node"] == parent), None)
         if not parent_node:
-            parent_node = {"name": parent, "children": []}
+            parent_node = {"node": parent, "children": []}
             entities.append(parent_node)
 
-        if len(words)==3:
+        if len(words) == 3:
             has_parent.append(child_node)
             parent_node["children"].append(child_node)
 
-
-    out=[entry for entry in entities if entry not in has_parent]
-    print(out )
+    return dict_to_paths([entry for entry in entities if entry not in has_parent])
 
 
+def
 
 
-input_text_to_dict(text2)
+print(dict_to_paths(input_text_to_dict(text)))
