a51f92bc34f6cd99795df3cf661063beaf37f63f
joanna <joanna.polewczyk@o2.pl>
2020-04-20 15:13:49 +0200

    animation graph

--- plotly_visualization/animation_graph.py
+++ plotly_visualization/animation_graph.py
@@ -1,63 +1,40 @@
-import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
 
-import plotly_visualization.balls_to_json as util
-
-
-def plot_balls(outfolder_path):
-    ball = util.balls_to_object(f"{outfolder_path}/reduced_nballs_after.txt")
-
-    df = pd.DataFrame([t.__dict__ for t in ball])
-    df[['x', 'y']] = pd.DataFrame(df.vector.values.tolist(), index=df.index)
-    df['x0'] = df.x - df.radius
-    df['x1'] = df.x + df.radius
-    df['y0'] = df.y - df.radius
-    df['y1'] = df.y + df.radius
-
-    final_df = df.sort_values(by=['radius'], ascending=False)
-
-    circles = []
-    for b in ball:
-        color = np.random.choice(range(256), size=3)
-        circles.append(dict(
-            type="circle",
-            xref="x",
-            yref="y",
-            x0=b.vector[0] - b.radius,
-            y0=b.vector[1] - b.radius,
-            x1=b.vector[0] + b.radius,
-            y1=b.vector[1] + b.radius,
-            line_color="rgb(" + str(color[0]) + ", " + str(color[1]) + ", " + str(color[2]) + ")",
-        ))
-
-    return final_df, circles
 
 def frame_args(duration):
     return {
-            "frame": {"duration": duration + 300},
-            "mode": "immediate",
-            "fromcurrent": True,
-            "transition": {"duration": duration, "easing": "quadratic-in-out"},
-        }
+        "frame": {"duration": duration + 300},
+        "mode": "immediate",
+        "fromcurrent": True,
+        "transition": {"duration": duration, "easing": "quadratic-in-out"},
+    }
 
 
 def animation_graph_plot(list):
+    print("list")
+    print(list)
     frames = []
+    step_names = []
+    last_circles = None
+    last_x = None
+    last_y = None
+    last_label = None
 
-    final_df, circles = plot_balls(f"out/14679")
-    print(final_df.to_string)
-    generate_frame(circles, final_df, frames, 0)
+    for index, step in enumerate(list):
+        step_names.append(index)
+        last_circles, last_x, last_y, last_label = generate_frame(step, frames, index)
 
     fig = go.Figure(
+        frames=frames,
         data=[go.Scatter(
-                        x=final_df["x"],
-                         y=final_df["y0"] - (final_df["radius"] * 0.05),
-                         text=final_df["word"],
+                        x=last_x,
+                         y=last_y,
+                         text=last_label,
                          mode="text",
                          )],
         layout=go.Layout(
-            shapes=circles,
+            shapes= [],
             autosize=True,
             yaxis=dict(
                 scaleanchor="x",
@@ -77,9 +54,12 @@ def animation_graph_plot(list):
                 pad=4
             ),
         ),
-        frames=frames,
-
     )
+
+    print(frames)
+    for k, f in enumerate(fig.frames):
+        print(f.name)
+        print(str(k))
 
     sliders = [
         {
@@ -99,6 +79,8 @@ def animation_graph_plot(list):
     ]
 
     fig.update_layout(
+        sliders=sliders,
+        shapes=[],
         updatemenus=[
             {
                 "buttons": [
@@ -120,28 +102,63 @@ def animation_graph_plot(list):
                 "y": 0,
             }
         ],
-        sliders=sliders
+        autosize=True,
+        yaxis=dict(
+            scaleanchor="x",
+            scaleratio=1,
+            showline=False,
+        ),
+        xaxis=dict(
+            showline=False,
+        ),
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0,
+            pad=4
+        ),
     )
-
-
-
-    # fig.show(config={'scrollZoom': True})
+    fig.show(config={'scrollZoom': True})
 
     return fig
 
 
+def generate_frame(step, frames, frame_name):
+    x = []
+    y = []
+    labels = []
 
+    circles = []
+    for k, b in step.items():
+        x.append(b.vector[0])
+        y.append(b.vector[1] - b.radius - (b.radius * 0.05))
+        labels.append(k)
+        color = np.random.choice(range(256), size=3)
+        circles.append(dict(
+            type="circle",
+            xref="x",
+            yref="y",
+            x0=b.vector[0] - b.radius,
+            y0=b.vector[1] - b.radius,
+            x1=b.vector[0] + b.radius,
+            y1=b.vector[1] + b.radius,
+            line_color="rgb(" + str(color[0]) + ", " + str(color[1]) + ", " + str(color[2]) + ")",
+        ))
 
-def generate_frame(circles, final_df, frames, frame_name):
     frames.append(go.Frame(data=[go.Scatter(
-        x=final_df["x"],
-        y=final_df["y0"] - (final_df["radius"] * 0.05),
-        text=final_df["word"],
+        x=x,
+        y=y,
+        text=labels,
         mode="text",
     )],
+        name=str(frame_name),
         layout=go.Layout(
             shapes=circles,
             # autosize=True,
-        ),
-        name=str(frame_name)
+        )
     ))
+
+    return circles, x, y, labels
